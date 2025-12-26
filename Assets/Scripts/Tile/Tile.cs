74a89c29e78bc854ce11769a2e52461651bf6721
MySun <sunhongjae@gmail.com>
2025-12-26 16:45:51 +0900

    Update Tile.cs

--- Assets/Scripts/Tile/Tile.cs
+++ Assets/Scripts/Tile/Tile.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
 
-public class T : MonoBehaviour
+public class Tile : MonoBehaviour
 {
     public TileData tileData;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
